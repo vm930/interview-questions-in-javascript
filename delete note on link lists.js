e784658c62d519fcee8b6a350e99845436bdc265
Victoria Mei <vm930@hotmail.com>
2019-08-13 15:58:05 -0400

    added a new link list question from leetcode

---
+++ delete note on link lists.js
@@ -0,0 +1,9 @@
+// Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+// Given linked list -- head = [4,5,1,9], which looks like following:
+
+//Input: head = [4,5,1,9], node = 5
+// Output: [4, 1, 9]
+// Input: head = [4, 5, 1, 9], node = 1
+// Output: [4, 5, 9]
+
+function deleteNote(node) {}
