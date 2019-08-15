7edb33136032915c7d9f8feaa576331333588889
Victoria Mei <vm930@hotmail.com>
2019-08-14 21:27:38 -0400

    a leetcode problem

--- Rotate Array.js
+++ Rotate Array.js
@@ -1,1 +1,20 @@
 //Leetcode Rotate Array
+// Input: [1, 2, 3, 4, 5, 6, 7] and k = 3
+// Output: [5, 6, 7, 1, 2, 3, 4]
+
+// Input: [-1, -100, 3, 99] and k = 2
+// Output: [3, 99, -1, -100]
+
+var rotate = function(nums, k) {
+	//splice the array from (0 to k) and store in a variable
+	//push the variable to the end of the array; subtract k-=1
+	while (k >= 0) {
+		let temp = nums.splice(0, 1);
+		nums.push(temp[0]);
+		k--;
+	}
+	console.log(nums);
+};
+
+rotate([ 1, 2, 3, 4, 5, 6, 7 ], 3);
+rotate([ -1, -100, 3, 99 ], 2);
---
+++ Valid Palindrome.js
@@ -0,0 +1,20 @@
+//Valid Palindrome
+// Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+//Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+//         Example 1:
+
+// Input: "A man, a plan, a canal: Panama"
+// Output: true
+// Example 2:
+
+// Input: "race a car"
+// Output: false
+
+var isPalindrome = function(s) {
+	//convert everything to lowwer case
+	//need to get ride of space and characters
+	//store in an array
+	//use hash obj to keep key as letter,
+};
