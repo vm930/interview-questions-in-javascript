36089b7cc6123eab1f18edd81458088adb73a2a8
Victoria Mei <vm930@hotmail.com>
2019-08-15 15:06:01 -0400

    finish

--- Rotate Array.js
+++ Rotate Array.js
@@ -6,14 +6,32 @@
 // Output: [3, 99, -1, -100]
 
 var rotate = function(nums, k) {
-	//splice the array from (0 to k) and store in a variable
-	//push the variable to the end of the array; subtract k-=1
-	while (k >= 0) {
-		let temp = nums.splice(0, 1);
-		nums.push(temp[0]);
-		k--;
+	//[1,2,3,4,5,6,7] and k = 3
+	//[5,6,7,1,2,3,4]
+
+	// step1 reverse all the numbers [7,6,5,4,3,2,1] k=3
+	// step2 reverse the k numbers [5,6,7,4,3,2,1]
+	// step3 reverse rest [5,6,7,1,2,3,4]
+
+	let newNums = reverse(nums, 0, nums.length - 1);
+	reverse(newNums, 0, k - 1);
+	reverse(newNums, k, newNums.length - 1);
+
+	function reverse(arr, startIndx, lastIndx) {
+		let i = lastIndx;
+		let j = startIndx;
+
+		while (j < i) {
+			let temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			i--;
+			j++;
+		}
+		return arr;
 	}
-	console.log(nums);
+	// console.log(arr);
+	// console.log(newNums);
 };
 
 rotate([ 1, 2, 3, 4, 5, 6, 7 ], 3);
