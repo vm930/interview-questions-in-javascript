118e9a2c300ed4bf9108d0511f408f36ac3e13b2
Victoria Mei <vm930@hotmail.com>
2019-08-16 15:38:37 -0400

    link list solution

--- 206. Reverse Linked List.js
+++ 206. Reverse Linked List.js
@@ -6,4 +6,34 @@
 // Input: 1 -> 2 -> 3 -> 4 -> 5 -> NULL
 // Output: 5 -> 4 -> 3 -> 2 -> 1 -> NULL
 
-function reverseList(head) {}
+var reverseList = function(head) {
+	// Input:   1->2->3->4->5->NULL
+	// Output:  5->4->3->2->1->NULL
+
+	//null <- 1 <- 2 <- 3 <- 4 <-5
+
+	//preval = null  2
+	//current = head  3
+	//nextval = head.next 4
+
+	let preval = null;
+	let current = head;
+	let nextval = head.next;
+
+	// 1<-2<-3<-4  preval
+	// null <-5    current
+
+	while (nextval !== null) {
+		current.next = preval;
+		preval = current;
+		current = nextval;
+		nextval = nextval.next;
+		// console.log(preval)
+		// console.log(current)
+		// console.log(nextval)
+	}
+	// preval.next = null;
+	current.next = preval;
+	// console.log(current);
+	return current;
+};
