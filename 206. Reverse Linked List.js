24b56b567080833bf148eba186581eb8f19f4e15
Victoria Mei <vm930@hotmail.com>
2019-08-17 21:57:41 -0400

    small modifications

--- 206. Reverse Linked List.js
+++ 206. Reverse Linked List.js
@@ -1,20 +1,14 @@
 // 206. Reverse Linked List
-
 // Reverse a singly linked list.
 //Example:
 
-// Input: 1 -> 2 -> 3 -> 4 -> 5 -> NULL
+// Input:  1 -> 2 -> 3 -> 4 -> 5 -> NULL
 // Output: 5 -> 4 -> 3 -> 2 -> 1 -> NULL
+
+//time o(n)
+//space o(1)
+
 var reverseList = function(head) {
-	// Input:   1->2->3->4->5->NULL
-	// Output:  5->4->3->2->1->NULL
-
-	//null <- 1 <- 2 <- 3 <- 4 <-5
-
-	//preval = null  2
-	//current = head  3
-	//nextval = head.next 4
-
 	let preval = null;
 	let current = head;
 	// let nextval = head.next
@@ -36,4 +30,11 @@ var reverseList = function(head) {
 	// current.next = preval;
 	// console.log(preval);
 	return preval;
+
+	while (current !== null) {
+		let nextval = current.next;
+		current.next = preval;
+		preval = current;
+		current = nextval;
+	}
 };
